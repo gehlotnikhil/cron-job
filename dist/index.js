3583d04ff65eaa5431508c230b06c5b1dd328290
gehlotnikhil <111073969+gehlotnikhil@users.noreply.github.com>
2025-02-09 15:07:55 +0530

    third

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,21 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const cron = require("node-cron");
-const 
 // Schedule the job to run at 12:00 AM daily
-cron, schedule;
-("52 22 * * *", () => {
-    console.log("Running the scheduled task at 12:00 AM!");
-    // Your task (e.g., send notifications, update DB)
-});
+const ServerUrl = process.env.ServerUrl || "http://localhost:8000";
+cron.schedule("* * * * *", () => __awaiter(void 0, void 0, void 0, function* () {
+    let result = yield fetch(`${ServerUrl}/api/dailynewproblem/delete`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+    });
+    let jsondata = yield result.json();
+    console.log(jsondata);
+}));
