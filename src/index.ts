784acf79ab43dc5c9a02e4b54f31fda10103820b
gehlotnikhil <111073969+gehlotnikhil@users.noreply.github.com>
2025-02-09 14:59:44 +0530

    issue resolved

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const cron = require("node-cron");
 // Schedule the job to run at 12:00 AM daily
-const  ServerUrl =  process.env.ServerUrl||"http://localhost:8000"
+const  ServerUrl =  process.env.ServerUrl||"http://localhost:8000";
 cron.schedule("* * * * *", async() => {
     let result = await fetch(`${ServerUrl}/api/dailynewproblem/delete`,{
         method: "DELETE",
@@ -9,5 +9,4 @@ cron.schedule("* * * * *", async() => {
     let jsondata = await result.json()
     console.log(jsondata);
     
-    // Your task (e.g., send notifications, update DB)
 });
